c6e63a6e55910adfd07c5f30d79dbb89c8b1aaf3
Bruno Ritter <ritter.brf@gmail.com>
2023-08-05 14:00:13 -0300

    add arguments functionality

--- freeze_playlist.py
+++ freeze_playlist.py
@@ -1,23 +1,38 @@
 import spotipy
 import os
+import argparse
 from spotipy.oauth2 import SpotifyOAuth
 
-new_playlist_name = "Mix BR"
-playlist_id = "37i9dQZF1E37KkzBQqMg3b"
 
-sp = spotipy.Spotify(
-    auth_manager=SpotifyOAuth(
-        client_id=os.getenv("client_id"),
-        client_secret=os.getenv("client_secret"),
-        redirect_uri="http://127.0.0.1:9090",
-        scope="playlist-read-private playlist-modify-public",
+def create_playlist(playlist_id, new_playlist_name):
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            # client_id=os.getenv("client_id"),
+            # client_secret=os.getenv("client_secret"),
+            redirect_uri="http://127.0.0.1:9090",
+            scope="playlist-read-private playlist-modify-public",
+        )
     )
-)
 
-tracks = sp.playlist_tracks(playlist_id)
-user_id = sp.me()["id"]
-new_playlist = sp.user_playlist_create(user_id, new_playlist_name)
-track_ids = [track["track"]["id"] for track in tracks["items"]]
-sp.playlist_add_items(new_playlist["id"], track_ids)
-shared_link = new_playlist["external_urls"]["spotify"]
-print(f"Shareable link: {shared_link}")
+    tracks = sp.playlist_tracks(playlist_id)
+    user_id = sp.me()["id"]
+    new_playlist = sp.user_playlist_create(user_id, new_playlist_name)
+    track_ids = [track["track"]["id"] for track in tracks["items"]]
+    sp.playlist_add_items(new_playlist["id"], track_ids)
+    shared_link = new_playlist["external_urls"]["spotify"]
+    print(f"Shareable link: {shared_link}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create a new playlist from an existing playlist ID."
+    )
+    parser.add_argument(
+        "playlist_id", type=str, help="The ID of the existing playlist."
+    )
+    parser.add_argument(
+        "new_playlist_name", type=str, help="The name for the new playlist."
+    )
+
+    args = parser.parse_args()
+    create_playlist(args.playlist_id, args.new_playlist_name)
